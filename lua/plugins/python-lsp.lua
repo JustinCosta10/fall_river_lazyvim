e22eb6ff1890656ea795ac6b30627d7053d2fc2e
Justin Costa <justincosta10@gmail.com>
2025-12-14 00:57:16 -0700

    changed to pyright

--- lua/plugins/python-lsp.lua
+++ lua/plugins/python-lsp.lua
@@ -3,15 +3,12 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        basedpyright = {
+        pyright = {
           settings = {
-            basedpyright = {
-              typeCheckingMode = "basic",
-              reportUnknownMemberType = "none",
-              reportUnknownVariableType = "none",
-              reportUnknownArgumentType = "none",
-              reportUnknownParameterType = "none",
-              reportMissingTypeStubs = "none",
+            python = {
+              analysis = {
+                typeCheckingMode = "standard",
+              },
             },
           },
         },
